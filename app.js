13e0cc75a1bd2495d031118767a1aec23faf08e8
MohamedMassoud10 <massoudfcai@gmail.com>
2023-07-02 03:23:38 +0300

    Handling GET request

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server !');
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from the server !', app: 'NATOURS' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post anything on the server');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
