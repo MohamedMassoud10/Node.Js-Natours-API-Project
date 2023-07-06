7dcc55b7dfd2217cbbb43be8fe92907365273e83
MohamedMassoud10 <massoudfcai@gmail.com>
2023-07-06 22:43:43 +0300

    Connecting Our Database with the Express App

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, // Add this line for the new MongoDB driver
+  })
+  .then((con) => {
+    console.log(con.Connection);
+    console.log('DB Connection Successful !');
+  });
+
 const app = require('./app');
-console.log(process.env);
-const port = 27017;
+const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
 });
