1d33ea422530fc6b26f129fed2de04429e2d4271
MohamedMassoud10 <massoudfcai@gmail.com>
2023-07-06 20:35:34 +0300

    try to make my first database

--- app.js
+++ app.js
@@ -22,4 +22,3 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
-//comment to commit
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 console.log(process.env);
-const port = 3000;
+const port = 27017;
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
 });
