9121abae13e9d3e17e439650a7b1e3ea5833b8c4
MohamedMassoud10 <massoudfcai@gmail.com>
2023-07-08 03:47:44 +0300

    am still tying to deal with mongoDB

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
-  .connect(DB, {
+  .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true, // Add this line for the new MongoDB driver
+    useUnifiedTopology: true,
   })
-  .then((con) => {
-    console.log(con.Connection);
-    console.log('DB Connection Successful !');
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
   });
 
 const app = require('./app');
