db51c0f558f89c4301b6cc810756f21556a5293c
MohamedMassoud10 <massoudfcai@gmail.com>
2023-07-08 06:43:26 +0300

    fiald to connect with atls

--- server.js
+++ server.js
@@ -2,22 +2,29 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-// const DB = process.env.DATABASE.replace(
-//   '<password>',
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
 
-mongoose
-  .connect(
-    'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then((con) => {
-    console.log('here we go connected !');
-  });
+// mongoose
+//   .connect(
+//     'mongodb+srv://mohamed:<WONOSlwc5fu604gF>@natours.uweji0s.mongodb.net/?retryWrites=true&w=majority',
+//     {
+//       useNewUrlParser: true,
+//     }
+//   )
+//   .then(() => {
+//     console.log('here we go connected !');
+//   });
+
+const uri = 'mongodb://127.0.0.1:27017/natours-text';
+mongoose.connect(uri);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function callback() {
+  console.log('connected successfully');
+});
 
 const app = require('./app');
 const port = 3000;
