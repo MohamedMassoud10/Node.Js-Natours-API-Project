3de9fb5d4d11e5be547099fee811d239705fe634
MohamedMassoud10 <massoudfcai@gmail.com>
2023-08-27 18:08:51 +0300

    add another two vaildation

--- models/tourModel.js
+++ models/tourModel.js
@@ -23,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty either : easy , medium and difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'the rating must be above 1.0'],
+      max: [5, 'the rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
