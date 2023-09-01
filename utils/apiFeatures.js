b88c6f4ce2dbba536cba5efaf15a3741dc3581cf
MohamedMassoud10 <massoudfcai@gmail.com>
2023-09-01 23:24:53 +0300

    api features error fixed

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -3,27 +3,29 @@ class APIFeatures {
     this.query = query;
     this.queryString = queryString;
   }
-  Filter() {
+
+  filter() {
     const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
     excludedFields.forEach((el) => delete queryObj[el]);
 
+    // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|lte|gt|le)\b/g, (match) => `$${match}`);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    this.query.find(JSON.parse(queryStr));
-    //let query = Tour.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
+
     return this;
   }
 
-  Sort() {
+  sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort('-createdAt');
     }
+
     return this;
   }
 
@@ -34,6 +36,7 @@ class APIFeatures {
     } else {
       this.query = this.query.select('-__v');
     }
+
     return this;
   }
 
@@ -41,7 +44,9 @@ class APIFeatures {
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
+
     this.query = this.query.skip(skip).limit(limit);
+
     return this;
   }
 }
