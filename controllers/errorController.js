1d5c7cd5f03790b6898fcb3d10a3f75cb48dbb97
MohamedMassoud10 <massoudfcai@gmail.com>
2023-09-02 00:50:09 +0300

    error in production and development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,35 @@
+sendErrorDev = (req, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+sendErrorProd = (req, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log('ERORR', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong !',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(req, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(req, res);
+  }
 };
