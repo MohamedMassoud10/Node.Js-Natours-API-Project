6693854c5ac5835687ab47f78fe08651f066c68e
MohamedMassoud10 <massoudfcai@gmail.com>
2023-09-06 10:28:01 +0300

    uncaughtException error

--- server.js
+++ server.js
@@ -24,3 +24,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+console.log(x);
