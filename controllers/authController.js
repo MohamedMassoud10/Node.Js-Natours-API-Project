202d37208417b201e38ee4b1063d3e67ae10977e
MohamedMassoud10 <massoudfcai@gmail.com>
2023-09-26 05:07:07 +0300

    geting started with protecting the routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,3 +47,20 @@ exports.login = catchAsync(async (req, res, next) => {
   });
   next();
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  console.log('in protect ');
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('you are not logged in ! please login to get access!', 401)
+    );
+  }
+});
